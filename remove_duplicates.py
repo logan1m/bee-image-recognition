16df415a2807fdb74b7d093b0c54ab5891d2a335
logan1m <89606272+logan1m@users.noreply.github.com>
2023-01-05 14:51:32 -0600

    Add files via upload

---
+++ remove_duplicates.py
@@ -0,0 +1,15 @@
+from difPy import dif
+search = dif(r"C:\Users\logan\Documents\Data Mining\Project\images\carpenter_bees")
+
+search.result
+#375 duplicate images
+
+'''Command Prompts'''
+#Get-ChildItem -Path ".\*.jpg" -Recurse | Move-Item -Destination "C:\Users\logan\Documents\Data Mining\Project\images\carpenter_bees"
+# transfer all jpg file types in current folder to other folder
+#pwd = get path
+#ls = get everything in folder
+#rm  = remove folder
+#cd = current directory
+#cd.. move back directory
+# cd [folder] = move into directory
